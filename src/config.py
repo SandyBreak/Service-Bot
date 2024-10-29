75c3e6c070578d78ff0ec1a9b8b2852742857df0
VSS <sandybreak3405@gmail.com>
2024-10-29 17:55:09 +0300

    ADD config.py

---
+++ src/config.py
@@ -0,0 +1,15 @@
+# -*- coding: UTF-8 -*-
+
+import os
+
+TELEGRAM_TOKEN = os.environ.get('TELEGRAM_TOKEN')
+
+LIST_USERS_TO_NEWSLETTER = []
+
+POSTGRES_USER = os.environ.get('POSTGRES_USER')
+POSTGRES_PASSWORD = os.environ.get('POSTGRES_PASSWORD')
+POSTGRES_HOST = os.environ.get('POSTGRES_HOST')
+POSTGRES_PORT = os.environ.get('POSTGRES_PORT')
+POSTGRES_DB = os.environ.get('POSTGRES_DB')
+
+DATABASE_URL = f"postgresql+asyncpg://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{int(POSTGRES_PORT)}/{POSTGRES_DB}"
